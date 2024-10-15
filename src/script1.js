2dccfe045d411882833f8d8628f441f934feb48a
dkeppel <Sam-Trathen@live.nmit.ac.nz>
2024-10-16 10:54:12 +1300

    Firebase API

--- src/script1.js
+++ src/script1.js
@@ -1,6 +1,26 @@
 //firebase config 
-//initialise firebase
+// Your web app's Firebase configuration
 
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDfwbdmlQl7zX9BDnoCYpWC01RLeBf189A",
+  
+    authDomain: "my-project-b7492.firebaseapp.com",
+  
+    projectId: "my-project-b7492",
+  
+    storageBucket: "my-project-b7492.appspot.com",
+  
+    messagingSenderId: "31311538636",
+  
+    appId: "1:31311538636:web:e3c5e1b831692c4cabd569"
+  
+  };
+  
+// Initialise Firebase
+const app = initializeApp(firebaseConfig);
+const database = firebase.database();
+const todoRef = firebase.database().ref("todo");
 //Select DOM elements
 const addToDoButton = document.getElementById("add-todo-btn");
 const todoInput = document.getElementById("todo-input");
@@ -37,3 +57,9 @@ todoInput.addEventListener("keypress", (e) => {
         addToDoButton.click()
     }
 });
+
+//Event listener to Toggle Dark Mode
+darkToggle.addEventListener("Click", () => {
+    document.body.classList.toggle("dark-mode")
+});
+
